49873d54af8073c0ad1de93d45633c7c669e379f
Eduardo Schwanke <carvalhoe089@gmail.com>
2022-06-24 08:24:54 -0300

    backend finalizado

--- src/server.js
+++ src/server.js
@@ -5,12 +5,14 @@ const mongoose = require('mongoose')
 
 const routes = require('./routes')
 const connectToDatabase = require('./database')
+const cors = require("cors")
 
 connectToDatabase()
 
 const app = express()
 const port = 3333
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
